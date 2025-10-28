2624777b18c6895e9c5c87a92fda8199a888fda3
= <=>
2025-10-28 11:29:58 +0700

    single-activity login. issues with the second screen

--- NewOSNewProject/app/build.gradle.kts
+++ NewOSNewProject/app/build.gradle.kts
@@ -39,8 +39,16 @@ android {
     }
 }
 
-dependencies {
 
+/*dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}*/
+
+dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -50,6 +58,12 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.appcompat)
+    implementation(libs.androidx.legacy.support.v4)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -57,4 +71,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-}
+
+//    implementation "androidx.fragment:fragment-ktx:1.8.9"
+
+    }
--- NewOSNewProject/app/src/main/java/com/example/newosnewproject/TmpFragment.kt
+++ NewOSNewProject/app/src/main/java/com/example/newosnewproject/TmpFragment.kt
@@ -1,8 +1,5 @@
 package com.example.newosnewproject
 
-
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,79 +9,101 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
+import androidx.navigation.fragment.findNavController
 
-class User (val login: String, val password: String){
-    fun check_input (l_input: String, p_input: String): Int {
+class MainFragActivity : AppCompatActivity(R.layout.activity_main) {
 
-        return (if (l_input == login && p_input == password) 1 else 0)
-    }
-}
-
-class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .add(R.id.fragment_container_view, FirstFragment.newInstance("foo"))
-        }
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        // Set up the action bar for use with the NavController
+        setupActionBarWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
 
 class FirstFragment : Fragment(R.layout.fragment_first){
-  override fun onCreateView(
-      inflater: LayoutInflater,
-      container: ViewGroup?,
-      savedInstanceState: Bundle?,): View {
-      return inflater.inflate(R.layout.fragment_first, container, false)
-  }
 
-  override fun onViewCreated(
-      view: View,
-      savedInstanceState: Bundle?) {
-      super.onViewCreated(view, savedInstanceState)
-      val usr = User("katey", "1234")
+    private val login = "katey"
+    private val pswrd = "1234"
 
-      val emailfield: EditText = view.findViewById<EditText>(R.id.email)
-      val pswdfield: EditText = view.findViewById<EditText>(R.id.password)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?): View {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
 
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val emailfield: EditText = view.findViewById<EditText>(R.id.email)
+        val pswdfield: EditText = view.findViewById<EditText>(R.id.password)
+        val nextbutton:Button = view.findViewById<Button>(R.id.button_first)
+        nextbutton.setOnClickListener {
+            val l_input: String = emailfield.getText().toString()
+            val p_input: String = pswdfield.getText().toString()
+            onButtonPressed(l_input, p_input)
+        }
+    }
 
-      val nextbutton:Button = view.findViewById<Button>(R.id.button_first)
-      nextbutton.setOnClickListener{
-          val l_input: String = emailfield.getText().toString()
-          val p_input: String = pswdfield.getText().toString()
-          val verify: Int = usr.check_input(l_input, p_input)
+    fun onButtonPressed(l_input : String, p_input : String){
+        if (l_input == login && p_input == pswrd) {
+            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        } else {
+        Toast.makeText(context, "Login Failed!", Toast.LENGTH_SHORT).show();
+        }
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+    }
 
-
-          if (verify != 0){
-              //TODO
-          } else {
-              Toast.makeText(context, "Login Failed!", Toast.LENGTH_SHORT).show()
-          }
-      }
-  }
   companion object {
+      fun newInstance() =
+          FirstFragment().apply {
+              arguments = Bundle().apply {
 
-      fun newInstance (name:String): FirstFragment = FirstFragment().apply {  }
+              }
+          }
   }
-
 }
 
-class SecondFragment : Fragment(){
-  override fun onCreateView(
+class SecondFragment : Fragment(R.layout.fragment_second){
+    override fun onCreateView(
       inflater: LayoutInflater,
       container: ViewGroup?,
       savedInstanceState: Bundle?,): View {
       return inflater.inflate(R.layout.fragment_second, container, false)
-  }
+    }
 
-  override fun onViewCreated(
-      view: View,
-      savedInstanceState: Bundle?) {
-      super.onViewCreated(view, savedInstanceState)
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+        }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+    }
+    companion object {
+        fun newInstance() =
+            SecondFragment().apply {
+                arguments = Bundle().apply {
 
-      }
-
+                }
+            }
+    }
 }
